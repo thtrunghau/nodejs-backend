d389e0529053f47fe1773491261e266592fbd344
Trung Hau <52100025@student.tdtu.edu.vn>
2023-11-03 15:11:23 +0700

    0311

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,17 +1,26 @@
+const connection = require("../config/database");
 
-const getHomePage = (req, res) =>{
-    res.send("hello mtk eheheehe")
-}
+const getHomePage = (req, res) => {
+  let users = [];
+  connection.query("select *from Users u", function (err, results, fields) {
+    users = results;
+    console.log(">>>result homepage = ", results); // results contains rows returned by server
+    //console.log(">>>fileds = ",fields); // fields contains extra meta data about results, if available
+    console.log(">>>check result", users);
+    res.send(JSON.stringify(users));
+  });
+};
+// Sequelize ORM & Prisma & TypeORM
+const getA = (req, res) => {
+  res.send("more rout yeh");
+};
 
-const getA = (req,res) =>{
-    res.send("more rout yeh");
-}
+const getSample = (req, res) => {
+  res.render("sample");
+};
 
-const getSample = (req, res) =>{
-    res.render("sample");
-}
 module.exports = {
-    getHomePage,
-    getA,
-    getSample
-}
+  getHomePage,
+  getA,
+  getSample,
+};
--- src/server.js
+++ src/server.js
@@ -5,28 +5,24 @@ const configViewEngine = require("./config/viewEngine");
 const path = require('path');
 const { config } = require('dotenv');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 const webRoutes = require("./routes/web");
+const connection = require('./config/database')
 
 configViewEngine(app);
 app.use("/", webRoutes)
 
 //test mysql
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3307,
-  user: 'root',
-  database: 'trunghau',
-  password: '123456'
-});
 
-connection.query(
-  'select *from Users u',
-  function(err, results, fields) {
-    console.log(">>>result = ",results); // results contains rows returned by server
-    console.log(">>>fileds = ",fields); // fields contains extra meta data about results, if available
-  }
-);
+
+// connection.query(
+//   'select *from Users u',
+//   function(err, results, fields) {
+//     // users = results;
+//     console.log(">>>result = ",results); // results contains rows returned by server
+//     //console.log(">>>fileds = ",fields); // fields contains extra meta data about results, if available
+//   }
+// );
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
